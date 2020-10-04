d6851195108358b67e68affb5e47219d73c06dfe
Ifat-Fahim <ifatfahim007@gmail.com>
2020-10-04 21:46:06 +0600

    adding entry functionality

--- running-tracker/script.js
+++ running-tracker/script.js
@@ -1,1 +1,23 @@
+//Selectors
+const form = document.querySelector("form");
+let entries = [];
+//Event listeners
+form.addEventListener("submit", handleSubmit);
 
+//Functions
+function addNewEntry(newEntry) {
+    const entriesWrapper = document.getElementById("entries");
+    entriesWrapper.removeChild(entriesWrapper.firstElementChild);
+    const listItem = document.createElement("li");
+    const listItemValue = document.createTextNode(newEntry);
+    listItem.appendChild(listItemValue);
+    entriesWrapper.appendChild(listItem);
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const entry = Number(document.getElementById("entry").value);
+    if (!entry) return;
+    form.reset();
+    entries.push(entry);
+    addNewEntry(entry);
+}
