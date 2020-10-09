5afe18af36ee6ea666596ab4294cc43721f981af
Ifat-Fahim <ifatfahim007@gmail.com>
2020-10-09 16:57:09 +0600

    added total and average section

--- running-tracker/script.js
+++ running-tracker/script.js
@@ -1,10 +1,29 @@
 //Selectors
 const form = document.querySelector("form");
 let entries = [];
+
 //Event listeners
 form.addEventListener("submit", handleSubmit);
 
 //Functions
+function reducer(total, currentValue) {
+    return total + currentValue;
+}
+
+function calcTotal(entries) {
+    const totalValue = entries.reduce(reducer).toFixed(1);
+    document.getElementById("total").innerHTML = totalValue;
+    document.getElementById("progressTotal").innerText = totalValue;
+    document.getElementById("average").innerText = (
+        totalValue / entries.length
+    ).toFixed(1);
+}
+
+function weeklyHigh(entries) {
+    const high = Math.max(...entries).toFixed(1);
+    document.getElementById("high").innerText = high;
+}
+
 function addNewEntry(newEntry) {
     const entriesWrapper = document.getElementById("entries");
     entriesWrapper.removeChild(entriesWrapper.firstElementChild);
@@ -20,4 +39,6 @@ function handleSubmit(e) {
     form.reset();
     entries.push(entry);
     addNewEntry(entry);
+    calcTotal(entries);
+    weeklyHigh(entries);
 }
